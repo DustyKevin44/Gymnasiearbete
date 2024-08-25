23ecf4b5140631448f8c3cd2e525b78bcdb96dc3
Viggo Nordström <viggowildberry@gmail.com>
2024-08-25 17:40:22 +0200

    Fixed Commit Error

--- Game/SpeletGymnasiearbete/SpeletGymnasiearbete/obj/Debug/net8.0/SpeletGymnasiearbete.AssemblyInfo.cs
+++ Game/SpeletGymnasiearbete/SpeletGymnasiearbete/obj/Debug/net8.0/SpeletGymnasiearbete.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SpeletGymnasiearbete")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+76d34d60249b4d6f74d0ae6ed8e2d8697db99ae2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+923b5fa5e6df838d50b884f438e39dfb8038244a")]
 [assembly: System.Reflection.AssemblyProductAttribute("SpeletGymnasiearbete")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SpeletGymnasiearbete")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
