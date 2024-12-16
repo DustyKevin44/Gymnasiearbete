4a50b57a129ad6de55ce43558da394aadce6b807
Viggo Nordström <viggowildberry@gmail.com>
2024-12-16 22:34:40 +0100

    Added oneline initialization of GameObject and children

--- Game/SpeletGymnasiearbete/SpeletGymnasiearbete/Classes/Prototype/Entity.cs
+++ Game/SpeletGymnasiearbete/SpeletGymnasiearbete/Classes/Prototype/Entity.cs
@@ -6,8 +6,8 @@ using Microsoft.Xna.Framework.Graphics;
 #nullable enable
 namespace SpeletGymnasiearbete.Classes;
 
-public class GameObject {
-    private List<GameObject> _children = [];
+public class GameObject(List<GameObject>? children=null) {
+    private List<GameObject> _children = children ?? [];
     protected GameObject? parent;
 
     public ref List<GameObject> GetChildren() {
@@ -38,7 +38,7 @@ public class GameObject {
 }
 
 
-public class Entity : GameObject {
+public class Entity(List<GameObject>? children=null) : GameObject(children) {
     public Vector2 Local_position;
     public Vector2 Global_position {
         get {
@@ -53,49 +53,41 @@ public class Entity : GameObject {
 }
 
 
-public class Sprite2D(Texture2D texture) : Entity {
+public class Sprite2D(Texture2D texture, List<GameObject>? children=null) : Entity(children) {
     public Texture2D Texture = texture;
 }
 
 
-// Components
+/*
 
-
-public abstract class AbstractComponent : GameObject;
-
-
-public class ScriptComponent(Action<GameTime, GameObject> script) : AbstractComponent {
-    private readonly Action<GameTime, GameObject> _script = script;
-
-    public new void Update(GameTime gameTime) {
-        if (parent is not null) _script.Invoke(gameTime, parent);
+Player
+{
+    Sprite
+    *AnimatedBehaviour
+    *FollowPositionBehaviour
+    StateMachine
+    {
+        IdleState --> *AnimatedBehaviour
+        FollowState --> *FollowPositionBehaviour
     }
 }
 
-
-// Behaviour
-
-
-/*
-
-Entity player = new(Vector2.Zero);
-player.AddChild(new Sprite(Vector2.Zero, texture));
-
-AnimatedBehaviour bobbingBehaviour = new(BobbingAnimation);
-player.AddChild(bobbingBehaviour);
-
-FollowPositionBehaviour follow_mouse = new(() => Mouse.get_position() + global.camera.position; );
-player.AddChild(followMouseBhaviour);
-
-StateMachine statemachine = new();
-statemachine.AddState(new IdleState(bobbingBehaviour));
-statemachine.AddState(new FollowState(followMouseBehaviour));
-player.AddChild(statemachine);
-
+Player player = new([
+    new Sprite(),
+    new CollisionShape([
+        new RectangleShape()
+    ]),
+    new AnimatedBehaviour(),
+    new FollowPositionBehaviour(),
+    new StateMachine([
+        new IdleState(),
+        new FollowState(),
+    ]),
+]);
 
 */
 
-public abstract class Behaviour(GameObject owner) : GameObject {
+public abstract class Behaviour(GameObject owner, List<GameObject>? children=null) : GameObject(children) {
     private readonly GameObject _owner = owner;
 
     public abstract void Execute(GameTime gameTime, GameObject owner);
@@ -147,7 +139,7 @@ public class IdleState(Behaviour[] behaviours) : IState {
 }
 
 
-public class StateMachineProt : AbstractComponent {
+public class StateMachineProt(List<GameObject>? children=null) : GameObject(children) {
     private readonly List<IState> _state = [];
 
     public void AddState(IState state) {
