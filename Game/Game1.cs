10c1f57323e2e4fa52b40d18b506cb2285538b6e
Viggo Nordström <viggowildberry@gmail.com>
2024-12-23 00:49:46 +0100

    Created runtime environment

--- Game/Game1.cs
+++ Game/Game1.cs
@@ -2,7 +2,7 @@
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 
-namespace Game;
+namespace Game1;
 
 public class Game1 : Game
 {
--- Game/Program.cs
+++ Game/Program.cs
@@ -1,2 +1,2 @@
-﻿using var game = new Game.Game1();
+﻿using var game = new Game1.Game1();
 game.Run();
