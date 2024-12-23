e53a2a15162764014c30b068bcf96d34ed558a39
Kevin <dustykevin44@gmail.com>
2024-12-23 02:04:04 +0100

    Added simple camera

--- Game/Game1.cs
+++ Game/Game1.cs
@@ -2,12 +2,39 @@
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 
+using MonoGame.Extended;
+
+using MonoGame.Extended.ViewportAdapters;
 namespace Game;
+
 
 public class Game : Microsoft.Xna.Framework.Game
 {
+    private Vector2 GetMovementDirection() // Move camera function
+{
+    var movementDirection = Vector2.Zero;
+    var state = Keyboard.GetState();
+    if (state.IsKeyDown(Keys.Down))
+    {
+        movementDirection += Vector2.UnitY;
+    }
+    if (state.IsKeyDown(Keys.Up))
+    {
+        movementDirection -= Vector2.UnitY;
+    }
+    if (state.IsKeyDown(Keys.Left))
+    {
+        movementDirection -= Vector2.UnitX;
+    }
+    if (state.IsKeyDown(Keys.Right))
+    {
+        movementDirection += Vector2.UnitX;
+    }
+    return movementDirection;
+}
     private GraphicsDeviceManager _graphics;
     private SpriteBatch _spriteBatch;
+    private OrthographicCamera _camera;
 
     public Game()
     {
@@ -21,6 +48,9 @@ public class Game : Microsoft.Xna.Framework.Game
         // TODO: Add your initialization logic here
 
         base.Initialize();
+
+         var viewportAdapter = new BoxingViewportAdapter(Window, GraphicsDevice, 800, 480);
+        _camera = new OrthographicCamera(viewportAdapter);
     }
 
     protected override void LoadContent()
@@ -36,6 +66,9 @@ public class Game : Microsoft.Xna.Framework.Game
             Exit();
 
         // TODO: Add your update logic here
+        // Camera Update
+        const float movementSpeed = -200;
+        _camera.Move(GetMovementDirection() * movementSpeed * gameTime.GetElapsedSeconds());
 
         base.Update(gameTime);
     }
@@ -45,6 +78,12 @@ public class Game : Microsoft.Xna.Framework.Game
         GraphicsDevice.Clear(Color.CornflowerBlue);
 
         // TODO: Add your drawing code here
+        // Camera logic
+         var transformMatrix = _camera.GetViewMatrix();
+        _spriteBatch.Begin(transformMatrix: transformMatrix);
+        _spriteBatch.DrawRectangle(new RectangleF(250,250,50,50), Color.Black, 1f);
+        _spriteBatch.End();
+
 
         base.Draw(gameTime);
     }
