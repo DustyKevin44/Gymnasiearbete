2de0b36e48acd063ab67090e2eab1498a7523cf6
Kevin <dustykevin44@gmail.com>
2025-01-21 19:47:04 +0100

    Finally fixed reference but nothing is drawn

--- Game/Custom/States/GameState.cs
+++ Game/Custom/States/GameState.cs
@@ -3,90 +3,91 @@ using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using Microsoft.Xna.Framework.Content;
 using MonoGame.Extended;
+using MonoGame.Extended.Tiled;
+using MonoGame.Extended.Tiled.Renderers;
 using MonoGame.Extended.ViewportAdapters;
 using Game.Custom.Input;
 using Game.Custom.Tilemap;
 
-namespace Game.Custom.States;
-
-public class GameState : State
+namespace Game.Custom.States
 {
-    private OrthographicCamera _camera;
-    private Hero _hero;
-    private Map _map;
-    private readonly GraphicsDeviceManager _graphics;
-
-    private static Vector2 GetMovementDirection()
+    public class GameState : State
     {
-        var state = Keyboard.GetState();
-        return new Vector2(
-            Utils.GetInputDirection(state.IsKeyDown(Keys.Left), state.IsKeyDown(Keys.Right)),
-            Utils.GetInputDirection(state.IsKeyDown(Keys.Up), state.IsKeyDown(Keys.Down))
-        );
-    }
+        private OrthographicCamera _camera;
+        private TiledMap _map;
+        private TiledMapRenderer _mapRenderer;
 
-    public GameState(Game game, GraphicsDevice graphicsDevice, ContentManager content)
-        : base(game, graphicsDevice, content)
-    {
-        Initialize();
-        LoadContent();
-    }
 
-    private void Initialize()
-    {
-        _map = new(_content);
-        _hero = new(_content.Load<Texture2D>("cursorButton"), Vector2.Zero);
-        Pathfinder.Init(_map, _hero);
+        // Utility function to get movement direction
+        private static Vector2 GetMovementDirection()
+        {
+            var state = Keyboard.GetState();
+            return new Vector2(
+                Utils.GetInputDirection(state.IsKeyDown(Keys.Left), state.IsKeyDown(Keys.Right)),
+                Utils.GetInputDirection(state.IsKeyDown(Keys.Up), state.IsKeyDown(Keys.Down))
+            );
+        }
 
-        var viewportAdapter = new BoxingViewportAdapter(_game.Window, _graphicsDevice, 500, 500);
-        _camera = new OrthographicCamera(viewportAdapter);
-    }
+        public GameState(Game game, GraphicsDevice graphicsDevice, ContentManager content)
+            : base(game, graphicsDevice, content)
+        {
+            Initialize();
+            LoadContent();
+        }
 
-    public void LoadContent()
-    {
+        private void Initialize()
+        {
+            // Load the Tiled map
+            _map = _content.Load<TiledMap>("tilemap"); // Use the name of your Tiled map file
 
-    // Load the map and pathfinding system
-    //var map = new Map(_content, _graphicsDevice);
-    //Pathfinder.Init(map.CollisionLayer);
-    }   
+            // Initialize the TiledMapRenderer
+            _mapRenderer = new TiledMapRenderer(_graphicsDevice);
 
-    public override void Update(GameTime gameTime)
-    { // Get elapsed time for smooth movement
-    float deltaTime = (float)gameTime.ElapsedGameTime.TotalSeconds;
+            var viewportAdapter = new BoxingViewportAdapter(_game.Window, _graphicsDevice, 500, 500);
+            _camera = new OrthographicCamera(viewportAdapter);
+        }
 
-    // Get the movement direction from input
-    var movementDirection = GetMovementDirection();
+        public void LoadContent()
+        {
+            // No content loading needed here for now
+        }
 
-    // Define camera speed
-    float cameraSpeed = 300f; // Adjust speed as necessary
+        public override void Update(GameTime gameTime)
+        {
+            // Get elapsed time for smooth movement
+            float deltaTime = (float)gameTime.ElapsedGameTime.TotalSeconds;
 
-    // Update the camera's position
-    _camera.Move(movementDirection * cameraSpeed * deltaTime);
+            // Get the movement direction from input
+            var movementDirection = GetMovementDirection();
 
-    // Update other game elements
-    var mouseState = Mouse.GetState();
-    var mousePosition = _camera.ScreenToWorld(mouseState.Position.ToVector2());
+            // Define camera speed
+            float cameraSpeed = 300f; // Adjust speed as necessary
 
-    //InputManager.Update();
+            // Update the camera's position with scaled movement direction
+            _camera.Move(movementDirection * cameraSpeed * deltaTime);
 
-    }
+            // Update other game elements
+            var mouseState = Mouse.GetState();
+            var mousePosition = _camera.ScreenToWorld(mouseState.Position.ToVector2());
+            // Do something with mousePosition if necessary
+        }
 
-    public override void Draw(GameTime gameTime, SpriteBatch spriteBatch)
-    {
-        // Camera logic
-        var transformMatrix = _camera.GetViewMatrix();
-        spriteBatch.Begin(transformMatrix: transformMatrix, sortMode: SpriteSortMode.Immediate, blendState: BlendState.AlphaBlend);
+        public override void Draw(GameTime gameTime, SpriteBatch spriteBatch)
+        {
+            // Camera logic
+            var transformMatrix = _camera.GetViewMatrix();
+            spriteBatch.Begin(transformMatrix: transformMatrix, sortMode: SpriteSortMode.Immediate, blendState: BlendState.AlphaBlend);
+            System.Console.WriteLine(transformMatrix);
 
-        // Draw hero and map
-        //_map.Draw(spriteBatch);
-        //_/hero.Draw(spriteBatch);
+            // Draw the Tiled map
+            _mapRenderer.Draw(transformMatrix); // Draw the map with camera transformation
 
-        spriteBatch.End();
-    }
+            spriteBatch.End();
+        }
 
-    public override void PostUpdate(GameTime gameTime)
-    {
-        // Example cleanup or post-update logic
+        public override void PostUpdate(GameTime gameTime)
+        {
+            // Example cleanup or post-update logic if needed
+        }
     }
 }
-
